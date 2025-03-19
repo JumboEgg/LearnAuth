6e2fc637909bf4976c0d76c7d00079b238793831
이한나 <giha0168@gmail.com>
2025-03-20 00:59:37 +0900

    BE/Feat/#5
    Create new repository

---
+++ Backend/src/main/java/ssafy/d210/backend/repository/SubLectureRepository.java
@@ -0,0 +1,7 @@
+package ssafy.d210.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ssafy.d210.backend.entity.SubLecture;
+
+public interface SubLectureRepository extends JpaRepository<SubLecture, Long> {
+}
---
+++ Backend/src/main/java/ssafy/d210/backend/service/CategoryService.java
@@ -0,0 +1,10 @@
+//package ssafy.d210.backend.service;
+//
+//import ssafy.d210.backend.repository.CategoryRepository;
+//
+//public class CategoryService {
+//
+//    private CategoryRepository categoryRepository;
+//
+//
+//}
