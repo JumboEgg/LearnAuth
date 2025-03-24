616d5f23a55bbfe6d17a64891809626471cd1c74
DESKTOP-4QCTMKG\SSAFY <okks3579@gmail.com>
2025-03-24 09:31:38 +0900

    로그인 기능 추가

--- Backend/src/main/java/ssafy/d210/backend/service/UserService.java
+++ Backend/src/main/java/ssafy/d210/backend/service/UserService.java
@@ -1,15 +1,21 @@
 package ssafy.d210.backend.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ssafy.d210.backend.dto.common.ResponseSuccessDto;
+import ssafy.d210.backend.dto.request.user.LoginRequest;
 import ssafy.d210.backend.dto.request.user.SignupRequest;
+import ssafy.d210.backend.dto.response.user.LoginResponse;
 import ssafy.d210.backend.dto.response.user.SignupResponse;
 import ssafy.d210.backend.entity.User;
 import ssafy.d210.backend.enumeration.response.HereStatus;
 import ssafy.d210.backend.repository.UserRepository;
 import ssafy.d210.backend.util.ResponseUtil;
+
+import java.util.List;
+
 
 @Service
 @RequiredArgsConstructor
@@ -33,5 +39,22 @@ public class UserService {
                 .build();
         ResponseSuccessDto<SignupResponse> res = responseUtil.successResponse(userSignupResponse, HereStatus.SUCCESS_SIGNUP);
         return res;
+    }
+
+    public ResponseSuccessDto<LoginResponse> login(LoginRequest loginRequest) {
+
+        List<User> userList = userRepository.findAll();
+        for (User user : userList) {
+            if (user.getEmail().equals(loginRequest.getEmail()) && user.getPassword().equals(loginRequest.getPassword())) {
+                LoginResponse loginResponse = LoginResponse.builder()
+                        .email(user.getEmail())
+                        .password(user.getPassword())
+                        .build();
+
+                ResponseSuccessDto<LoginResponse> res = responseUtil.successResponse(loginResponse, HereStatus.SUCCESS_LOGIN);
+                return res;
+            }
+        }
+        return null;
     }
 }
