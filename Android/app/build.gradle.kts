16ae8ca6d9973ff2c8a1ce9b9ff16916b3b18819
박진현 <pjiy1220@naver.com>
2025-03-26 17:08:43 +0900

    [Android/Feat/#18]: 수료증 더미 데이터 추가

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.navigation.safeargs) // SafeArgs 플러그인 추가
+    id("androidx.navigation.safeargs.kotlin")
+    id("kotlin-parcelize")
 }
 
 android {
