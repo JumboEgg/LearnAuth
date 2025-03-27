be84b1b8656400e11c58d316b770f5bec0b7df6b
박진현 <pjiy1220@naver.com>
2025-03-27 17:49:16 +0900

    [Android/Feat/#41]: 강의 전체 등록 카테고리 선택 수정

--- Android/app/src/main/java/com/example/second_project/ui/RegisterLectureFragment.kt
+++ Android/app/src/main/java/com/example/second_project/ui/RegisterLectureFragment.kt
@@ -1,10 +1,13 @@
 package com.example.second_project.ui
 
+import android.R
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -26,9 +29,15 @@ class RegisterLectureFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // 뒤로가기
-        binding.btnBack.setOnClickListener {
-            findNavController().popBackStack()
+        val categoryList = listOf("체육", "데이터", "법률", "생명과학", "마케팅") // ← 이건 백엔드에서 받아온 값으로 바꿀 수 있음
+
+        val adapter = ArrayAdapter(requireContext(), R.layout.simple_list_item_1, categoryList)
+        binding.autoCompleteCategory.setAdapter(adapter)
+
+        // 선택 이벤트 (선택한 항목 로그 찍기 등)
+        binding.autoCompleteCategory.setOnItemClickListener { _, _, position, _ ->
+            val selected = categoryList[position]
+            Log.d("CategorySelect", "선택된 카테고리: $selected")
         }
 
 
---
+++ Android/app/src/main/java/com/example/second_project/ui/RegisterUploadFragment.kt
@@ -0,0 +1,17 @@
+package com.example.second_project.ui
+
+import androidx.fragment.app.Fragment
+import android.R
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+
+class RegisterUploadFragment: Fragment() {
+
+}
