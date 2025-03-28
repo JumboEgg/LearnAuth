02d603e9d471f9f5bfb2e8f31d01b1ecba50aab7
박진현 <pjiy1220@naver.com>
2025-03-28 15:06:16 +0900

    [Android/Feat/#41]: 강의 등록 수익 정보 페이지 구현 중

--- Android/app/src/main/java/com/example/second_project/ui/RegisterPaymentFragment.kt
+++ Android/app/src/main/java/com/example/second_project/ui/RegisterPaymentFragment.kt
@@ -5,12 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.second_project.adapter.RegisterParticipantsAdapter
 import com.example.second_project.databinding.FragmentRegisterPaymentBinding
 
 class RegisterPaymentFragment: Fragment() {
 
     private var _binding: FragmentRegisterPaymentBinding? = null
     private val binding get() = _binding!!
+    private lateinit var adapter: RegisterParticipantsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +28,28 @@ class RegisterPaymentFragment: Fragment() {
         binding.btnToSubLecture.setOnClickListener {
             (parentFragment as? RegisterMainFragment)?.moveToStep(3)
         }
+
+        val participantNames = mutableListOf("홍길동", "김싸피")
+        val isLecturerFlags = mutableListOf(false, false)
+
+        val adapter = RegisterParticipantsAdapter(
+            participantNames,
+            onLecturerToggle = { position ->
+                // 여기에 "강의자 바뀜"에 대한 동작 작성
+            },
+            onDeleteClick = { position ->
+                participantNames.removeAt(position)
+                isLecturerFlags.removeAt(position)
+                adapter.notifyItemRemoved(position)
+            },
+            onNameClick = { position ->
+                // 다이얼로그 띄우기
+            },
+            isLecturerFlags = isLecturerFlags
+        )
+
+        binding.recyclerParticipants.adapter = adapter
+
     }
 
     override fun onDestroyView() {
