27a7bf92c2f94fe2bdb07e2bce4b80d7138c0e4a
이서현 <pado9589@gmail.com>
2025-03-28 17:06:59 +0900

    [Android/Feat/#59] : 로그인페이지 비밀번호 노출 여부 설정

--- Android/app/src/main/java/com/example/second_project/LoginActivity.kt
+++ Android/app/src/main/java/com/example/second_project/LoginActivity.kt
@@ -2,12 +2,15 @@ package com.example.second_project
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import androidx.appcompat.app.AppCompatActivity
 import com.example.second_project.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityLoginBinding
+    private var isPwVisible = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +27,24 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        binding.loginPwShow.setOnClickListener {
+            changePasswordVisibility()
+        }
+
+    }
+
+    private fun changePasswordVisibility() {
+        if (isPwVisible) {
+            binding.loginPw.transformationMethod = PasswordTransformationMethod.getInstance()
+            binding.loginPwShow.setImageResource(R.drawable.invisibleicon)
+        } else {
+            binding.loginPw.transformationMethod = HideReturnsTransformationMethod.getInstance()
+            binding.loginPwShow.setImageResource(R.drawable.visibleicon)
+        }
+        isPwVisible = !isPwVisible
+
+        // 커서 위치 유지
+        binding.loginPw.setSelection(binding.loginPw.text.length)
     }
 
 
