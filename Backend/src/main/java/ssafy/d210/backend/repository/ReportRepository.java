60611509bf054ef6feff470d93c1b61b7d8b768d
이한나 <giha0168@gmail.com>
2025-03-28 10:18:56 +0900

    BE/Feat/#44 : create report API

--- Backend/src/main/java/ssafy/d210/backend/repository/ReportRepository.java
+++ Backend/src/main/java/ssafy/d210/backend/repository/ReportRepository.java
@@ -3,5 +3,9 @@ package ssafy.d210.backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ssafy.d210.backend.entity.Report;
 
+import java.util.List;
+
 public interface ReportRepository extends JpaRepository<Report, Long> {
+    // UserLecture entity 내의 userid로 신고 목록 조회
+    List<Report> findByUserLectureUserId(long userId);
 }
