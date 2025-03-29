f8261722e75dfd9a36de5b5d3b93799fda3d3ce0
이한나 <giha0168@gmail.com>
2025-03-29 18:35:46 +0900

    BE/Feat/#55

--- Backend/src/main/java/ssafy/d210/backend/dto/request/lecture/LectureRegisterRequest.java
+++ Backend/src/main/java/ssafy/d210/backend/dto/request/lecture/LectureRegisterRequest.java
@@ -21,4 +21,22 @@ public class LectureRegisterRequest {
     private List<SubLectureRequest> subLectures = new ArrayList<>();
     private List<QuizRequest> quizzes = new ArrayList<>();
 
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getCategoryName() { return categoryName; }
+    public void setCategoryName(String categoryName) { this.categoryName = categoryName; }
+    public String getGoal() { return goal; }
+    public void setGoal(String goal) { this.goal = goal; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public int getPrice() { return price; }
+    public void setPrice(int price) { this.price = price; }
+    public String getWalletKey() { return walletKey; }
+    public void setWalletKey(String walletKey) { this.walletKey = walletKey; }
+    public List<RatioRequest> getRatios() { return ratios; }
+    public void setRatios(List<RatioRequest> ratios) { this.ratios = ratios; }
+    public List<SubLectureRequest> getSubLectures() { return subLectures; }
+    public void setSubLectures(List<SubLectureRequest> subLectures) { this.subLectures = subLectures; }
+    public List<QuizRequest> getQuizzes() { return quizzes; }
+    public void setQuizzes(List<QuizRequest> quizzes) { this.quizzes = quizzes; }
 }
--- Backend/src/main/java/ssafy/d210/backend/dto/request/payment/RatioRequest.java
+++ Backend/src/main/java/ssafy/d210/backend/dto/request/payment/RatioRequest.java
@@ -9,4 +9,11 @@ public class RatioRequest {
     private String email;
     private int ratio;
     private boolean lecturer;
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public int getRatio() { return ratio; }
+    public void setRatio(int ratio) { this.ratio = ratio; }
+    public boolean isLecturer() { return lecturer; }
+    public void setLecturer(boolean lecturer) { this.lecturer = lecturer; }
 }
