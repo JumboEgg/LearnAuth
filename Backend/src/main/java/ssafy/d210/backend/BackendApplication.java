ec5f7d498bb7f56a0206161d7f5666f335d4871e
DESKTOP-4QCTMKG\SSAFY <okks3579@gmail.com>
2025-03-30 20:15:30 +0900

    BE/Setting/#76 : 스웨거 수정완료

--- Backend/src/main/java/ssafy/d210/backend/BackendApplication.java
+++ Backend/src/main/java/ssafy/d210/backend/BackendApplication.java
@@ -1,13 +1,10 @@
 package ssafy.d210.backend;
 //
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(servers = {@Server(url = "https://j12d210.p.ssafy.io", description = "https"), @Server(url = "http://j12d210.p.ssafy.io:8080",description = "로컬")})
-public class BackendApplication {
+class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
