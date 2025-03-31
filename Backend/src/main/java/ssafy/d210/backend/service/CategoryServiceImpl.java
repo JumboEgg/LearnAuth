c57a8c19c6512f6d47c617037035d1b1986d8da8
이한나 <giha0168@gmail.com>
2025-03-31 23:52:57 +0900

    BE/Fix/#99 : Category Data Change

--- Backend/src/main/java/ssafy/d210/backend/service/CategoryServiceImpl.java
+++ Backend/src/main/java/ssafy/d210/backend/service/CategoryServiceImpl.java
@@ -3,7 +3,6 @@ package ssafy.d210.backend.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ssafy.d210.backend.dto.common.ResponseSuccessDto;
-import ssafy.d210.backend.dto.response.category.CategoryListResponse;
 import ssafy.d210.backend.dto.response.category.CategoryResponse;
 import ssafy.d210.backend.entity.Category;
 import ssafy.d210.backend.enumeration.response.HereStatus;
@@ -21,17 +20,14 @@ public class CategoryServiceImpl implements CategoryService{
     private final ResponseUtil responseUtil;
 
     @Override
-    public ResponseSuccessDto<CategoryListResponse> getCategoryList() {
+    public ResponseSuccessDto<List<CategoryResponse>> getAllCategoryList() {
         //DB 카테고리 전체 조회
         List<Category> categories = categoryRepository.findAll();
         // Category entity -> CategoryResponse DTO
         List<CategoryResponse> responseList = categories.stream()
                 .map(cat -> new CategoryResponse(cat.getId(), cat.getCategoryName().name()))
                 .collect(Collectors.toList());
-        // CategoryListResponse에 담기
-        CategoryListResponse categoryListResponse = new CategoryListResponse();
-        categoryListResponse.setCategoryList(responseList);
 
-        return responseUtil.successResponse(categoryListResponse, HereStatus.SUCCESS_CATEGORY_LIST);
+        return responseUtil.successResponse(responseList, HereStatus.SUCCESS_CATEGORY_LIST);
     }
 }
