a46b4374e951f6fcf4457f7d8bbec86a102d7901
이한나 <giha0168@gmail.com>
2025-03-31 17:52:52 +0900

    BE/Feat/#92 : Create Category API

---
+++ Backend/src/main/java/ssafy/d210/backend/service/CategoryService.java
@@ -0,0 +1,9 @@
+package ssafy.d210.backend.service;
+
+import ssafy.d210.backend.dto.common.ResponseSuccessDto;
+import ssafy.d210.backend.dto.response.category.CategoryListResponse;
+
+// 카테고리 조회 로직
+public interface CategoryService {
+    public ResponseSuccessDto<CategoryListResponse> getCategoryList();
+}
