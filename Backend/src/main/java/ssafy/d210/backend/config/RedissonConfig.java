c2ed71a8f3dd91c5b33864843429c72eb20bed7f
DESKTOP-4QCTMKG\SSAFY <okks3579@gmail.com>
2025-04-01 16:53:30 +0900

    BE/Setting

--- Backend/src/main/java/ssafy/d210/backend/config/RedissonConfig.java
+++ Backend/src/main/java/ssafy/d210/backend/config/RedissonConfig.java
@@ -6,6 +6,7 @@ import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class RedissonConfig {
@@ -16,13 +17,34 @@ public class RedissonConfig {
     @Value("${spring.data.redis.port}")
     public int port;
 
+    @Value("${spring.redis.password:}")  // 비밀번호가 없으면 빈 문자열
+    private String password;
+
+    @Value("${spring.redis.ssl:false}")  // SSL 사용 여부
+    private boolean useSsl;
+
     private static final String REDDISON_HOST_PREFIX= "redis://";
 
     @Bean
+    @Profile({"local"})
     public RedissonClient redissonClient() {
 
         Config config = new Config();
         config.useSingleServer().setAddress(REDDISON_HOST_PREFIX + host + ":" + port);
+
+        return Redisson.create(config);
+    }
+
+    @Bean
+    @Profile({"dev"})
+    public RedissonClient redissonClientDev() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress(REDDISON_HOST_PREFIX + host +":" + port)
+                .setPassword(password)
+                .setConnectTimeout(10000)
+                .setRetryAttempts(3)
+                .setRetryInterval(1500);
 
         return Redisson.create(config);
     }
