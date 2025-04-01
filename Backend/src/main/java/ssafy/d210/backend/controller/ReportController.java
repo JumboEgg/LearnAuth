d15498527dbf31f0f3201d7320b7ebef940e60d9
이한나 <giha0168@gmail.com>
2025-04-01 15:46:11 +0900

    BE/Fix/#108 : Report 내부 논리 수정

--- Backend/src/main/java/ssafy/d210/backend/controller/ReportController.java
+++ Backend/src/main/java/ssafy/d210/backend/controller/ReportController.java
@@ -6,18 +6,15 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import ssafy.d210.backend.dto.common.ResponseSuccessDto;
 import ssafy.d210.backend.dto.request.report.ReportRequest;
 import ssafy.d210.backend.dto.response.report.ReportDetailResponse;
 import ssafy.d210.backend.dto.response.report.ReportResponse;
-import ssafy.d210.backend.security.dto.CustomUserDetails;
 import ssafy.d210.backend.service.ReportService;
-import ssafy.d210.backend.service.ReportServiceImpl;
 
 import java.util.List;
-//
+
 @RestController
 @RequestMapping("/api/report")
 @RequiredArgsConstructor
@@ -38,15 +35,8 @@ public class ReportController {
             // @AuthenticationPrincipal CustomUserDetails userDetails
             @RequestParam("userId") Long userId
     ) {
-        // jwt : LonguserId = userDetails.getUserId();
-        // reportService에서 userId를 갖고 해당 유저의 신고 리스트를 가지고 온다.
-        List<ReportResponse> reports = reportService.getReports(userId);
         // 조회한 신고 리스트를 응답으로 반환한다.
-        return ResponseEntity.ok(
-                ResponseSuccessDto.<List<ReportResponse>>builder()
-                        .data(reports)
-                        .build()
-        );
+        return ResponseEntity.ok(reportService.getReports(userId));
     }
 
     // 받은 신고 자세히 보기 @GetMapping("/{reportId}")
@@ -59,13 +49,7 @@ public class ReportController {
     public ResponseEntity<ResponseSuccessDto<ReportDetailResponse>> getReportDetail(
             @PathVariable("reportId") Long reportId
     ) {
-        // 서비스에 해당 reportId 신고 데이터 요청, 데이터를 detail 변수에 담는다.
-        ReportDetailResponse detail = reportService.getReportDetail(reportId);
-        return ResponseEntity.ok(
-                ResponseSuccessDto.<ReportDetailResponse>builder()
-                        .data(detail)
-                        .build()
-        );
+        return ResponseEntity.ok(reportService.getReportDetail(reportId));
     }
 
     // 신고 하기 @PostMapping
@@ -76,18 +60,8 @@ public class ReportController {
     })
     public ResponseEntity<ResponseSuccessDto<Void>> submitReport(
             @RequestBody ReportRequest request
-            // JWT에서 가져온 사용자 정보
-//            @AuthenticationPrincipal CustomUserDetails userDetails
     ) {
-        // userId 꺼내기
-        // Long userId = userDetails.getUserId();
-
-        reportService.createReport(request, request.getUserId());
-        return ResponseEntity.ok(
-                ResponseSuccessDto.<Void>builder()
-                        .status("신고가 접수되었습니다.")
-                        .build()
-        );
+        return ResponseEntity.ok(reportService.createReport(request, request.getUserId()));
     }
 
 
