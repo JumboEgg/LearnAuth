97543432ad3e91617b8afe921b68c0e1ab5705f2
ghaud5684 <ghaud5684@naver.com>
2025-04-06 17:22:22 +0900

    BE/Fix/#168 : 강의 ã구매 request dto 형식 수정

--- Backend/src/main/java/ssafy/d210/backend/dto/request/lecture/PurchaseLectureRequest.java
+++ Backend/src/main/java/ssafy/d210/backend/dto/request/lecture/PurchaseLectureRequest.java
@@ -2,10 +2,13 @@ package ssafy.d210.backend.dto.request.lecture;
 
 import lombok.Getter;
 import lombok.Setter;
+import ssafy.d210.backend.dto.request.transaction.SignedRequest;
+
 //
 @Getter
 @Setter
 public class PurchaseLectureRequest {
     private long userId;
     private long lectureId;
+    private SignedRequest signedRequest;
 }
