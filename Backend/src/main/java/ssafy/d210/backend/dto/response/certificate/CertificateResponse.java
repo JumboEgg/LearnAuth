2333012576d5e9656fa8d22632b7ae642b89a32e
이한나 <giha0168@gmail.com>
2025-04-08 12:52:38 +0900

    BE/Fix/#202 : certificate 목록 조회 API data type 수정

--- Backend/src/main/java/ssafy/d210/backend/dto/response/certificate/CertificateResponse.java
+++ Backend/src/main/java/ssafy/d210/backend/dto/response/certificate/CertificateResponse.java
@@ -2,6 +2,9 @@ package ssafy.d210.backend.dto.response.certificate;
 
 import lombok.*;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 //
 @Getter
 @Builder
@@ -10,5 +13,15 @@ public class CertificateResponse {
     private String title;
     private String categoryName;
     private Integer certificate;
-    private String certificateDate;
+    // userlecture entity    private LocalDate certificateDate; 에 따른 데이터 형식 수정
+    private Date certificateDate;
+
+    // native query 사용시 필요
+    public CertificateResponse(long lectureId, String title, String categoryName, Integer certificate, Date certificateDate) {
+        this.lectureId = lectureId;
+        this.title = title;
+        this.categoryName = categoryName;
+        this.certificate = certificate;
+        this.certificateDate = certificateDate;
+    }
 }
